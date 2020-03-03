bb885d7d85db0ddbcb501af255b3cbeda128bada
EdJoPaTo <edjopato@gmail.com>
2020-03-03 10:46:43 +0100

    refactor(interface): more precise parse_mode typing

--- source/interface.ts
+++ source/interface.ts
@@ -1,5 +1,5 @@
 export interface Formatter {
-	parse_mode: string;
+	parse_mode: 'HTML' | 'Markdown';
 	bold: (text: string) => string;
 	escape: (text: string) => string;
 	italic: (text: string) => string;
